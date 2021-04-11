0dc2f86b68e2315bffa3ad4de3d2b963276b9722
edilbertojj65 <edilbertojj65@gmail.com>
2021-04-10 23:12:43 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+//Add your code here
+
+   function submitData( name, email ) {
+      let configObj = {
+       method: "POST",  
+       headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+       },
+          body: JSON.stringify({
+           name: name,
+           email: email
+        })
+       };
+  
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+    //  console.log(object);
+      document.body.innerHTML = object[ "id" ];
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+     // console.log(error.message);
+      document.body.innerHTML = error.message;
+    });
+
+}
